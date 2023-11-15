6a4e069461e17b4929c40570812b8e121d8abf20
Fernando <fernando.penaranda@globant.com>
2023-11-14 21:18:52 -0500

    first commit

---
+++ src/test/java/com/services/banktransactionservice/BankTransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.services.banktransactionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTransactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
