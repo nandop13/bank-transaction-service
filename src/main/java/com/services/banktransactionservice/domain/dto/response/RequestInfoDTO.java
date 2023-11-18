25959ba292867281fd93d7f607d0d287de90fe7d
Fernando <fernando.penaranda@globant.com>
2023-11-18 12:16:23 -0500

    WTS-001 add Response DTO will wrap and return the wallet Service data response

---
+++ src/main/java/com/services/banktransactionservice/domain/dto/response/RequestInfoDTO.java
@@ -0,0 +1,20 @@
+package com.services.banktransactionservice.domain.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.services.banktransactionservice.domain.dto.BankAccountDTO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class RequestInfoDTO {
+    private String status;
+}
