dc17dbb4a506473ca9a928afa9d184981ef5a950
Fernando <fernando.penaranda@globant.com>
2023-11-18 12:08:39 -0500

    WTS-001 add DTOs requests objects

---
+++ src/main/java/com/services/banktransactionservice/application/adapter/response/GetBalanceResponse.java
@@ -0,0 +1,21 @@
+package com.services.banktransactionservice.application.adapter.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class GetBalanceResponse {
+    private BigDecimal balance;
+    @JsonProperty(value = "user_id")
+    private Integer userId;
+}
