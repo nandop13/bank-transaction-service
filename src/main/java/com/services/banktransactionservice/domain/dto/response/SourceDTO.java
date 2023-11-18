e4a996c1ee1623492165a816a56c852f6e44ad21
Fernando <fernando.penaranda@globant.com>
2023-11-18 12:21:07 -0500

    WTS-001 add response object

---
+++ src/main/java/com/services/banktransactionservice/domain/dto/response/SourceDTO.java
@@ -0,0 +1,24 @@
+package com.services.banktransactionservice.domain.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.services.banktransactionservice.domain.dto.BankAccountDTO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class SourceDTO {
+    private String type;
+    @JsonProperty("sourceInformation")
+    private SourceInformationDTO sourceInformation;
+    @JsonProperty("account")
+    private BankAccountDTO bankAccountDTO;
+}
